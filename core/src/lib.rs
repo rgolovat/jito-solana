4d9ae6561bb0236856cde20d405a1824bb6bfdba
fuzzbuck <git-no-reply@io.software>
2025-04-12 10:47:35 +0200

    mev hook

--- core/src/lib.rs
+++ core/src/lib.rs
@@ -100,7 +100,7 @@ pub fn proto_packet_to_packet(p: jito_protos::proto::packet::Packet) -> Packet {
             if flags.repair {
                 packet.meta_mut().flags.insert(PacketFlags::REPAIR);
             }
-            if flags.from_staked_node {
+            if flags.tracer_packet {
                 packet
                     .meta_mut()
                     .flags
